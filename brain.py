b7263b9670d9c7256861b9c093774630079c8f12
ColeJMitchell <colejmitchell.cs@gmail.com>
2025-01-06 14:55:47 -0500

    stabilized the algorithm for line detection

--- brain.py
+++ brain.py
@@ -33,7 +33,7 @@ def receive_frame(client_socket, data_size):
 def main():
     try:
         client_vision = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-        client_vision.connect(('winddows ip', 5001))
+        client_vision.connect(('windows ip', 5001))
         data_size = struct.calcsize("Q")
         while True:
             try:
@@ -44,11 +44,12 @@ def main():
                 ones = np.ones((9, 9), np.uint8)
                 canny = cv2.Canny(result, 50, 100)
                 dilated = cv2.dilate(canny, ones, iterations=1)
-                hough = cv2.HoughLinesP(dilated, 1, np.pi/180, 100, minLineLength=600, maxLineGap=30)
+                hough = cv2.HoughLinesP(dilated, 1, np.pi/180, 100, minLineLength=650, maxLineGap=30)
                 if hough is not None:
                     for line in hough:
                         x1, y1, x2, y2 = line[0]
                         cv2.line(frame, (x1, y1), (x2, y2), (0, 255, 0), 5)
+                frame = frame[30:930, 638:1567]
                 cv2.imshow("frame", frame)
                 if cv2.waitKey(1) & 0xFF == ord('q'):
                     break
