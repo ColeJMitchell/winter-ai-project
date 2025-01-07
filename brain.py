205f9e063d68f45f118fd5305608542e4b9da07c
ColeJMitchell <colejmitchell.cs@gmail.com>
2025-01-07 11:51:27 -0500

    duplicate intersections are now filtered out

--- brain.py
+++ brain.py
@@ -1,5 +1,4 @@
 import socket
-import time
 import struct
 import pickle
 import cv2
@@ -29,6 +28,27 @@ def receive_frame(client_socket, data_size):
     frame = pickle.loads(frame_data)
     return frame
 
+# Finds the intersection points of the vertical and horizontal lines
+def findIntersection(x1,y1,x2,y2,x3,y3,x4,y4):
+    denom = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4)
+    px = ((x1 * y2 - y1 * x2) * (x3 - x4) - (x1 - x2) * (x3 * y4 - y3 * x4)) / denom
+    py = ((x1 * y2 - y1 * x2) * (y3 - y4) - (y1 - y2) * (x3 * y4 - y3 * x4)) / denom
+    return px, py
+
+# Filters out points that are too close to each other
+def distance(points, threshold):
+    filtered = []
+    while len(filtered) != 81:
+        x, y = points[0]
+        for i in range(len(points) - 1, 0, -1):  
+            x2, y2 = points[i]
+            if abs(x2 - x) < threshold and abs(y2 - y) < threshold:
+                points.pop(i)  
+        filtered.append(points.pop(0))  
+    return filtered
+
+
+
 # Connects to the server and processes the video feed
 def main():
     try:
@@ -45,11 +65,28 @@ def main():
                 canny = cv2.Canny(result, 50, 100)
                 dilated = cv2.dilate(canny, ones, iterations=1)
                 hough = cv2.HoughLinesP(dilated, 1, np.pi/180, 100, minLineLength=650, maxLineGap=30)
+                vertical = []
+                horizontal = []
                 if hough is not None:
                     for line in hough:
                         x1, y1, x2, y2 = line[0]
-                        cv2.line(frame, (x1, y1), (x2, y2), (0, 255, 0), 5)
+                        #cv2.line(frame, (x1, y1), (x2, y2), (0, 255, 0), 1)
+                        if abs(x2 - x1) > abs(y2 - y1):
+                            horizontal.append((x1, y1, x2, y2))
+                            cv2.line(frame, (x1, y1), (x2, y2), (0, 255, 0), 1)
+                        if abs(y2-y1) > abs(x2 - x1):
+                            vertical.append((x1, y1, x2, y2))
+                            cv2.line(frame, (x1, y1), (x2, y2), (255, 0, 0), 1)
+                points = []
+                for hlines in horizontal:
+                    for vlines in vertical:
+                        x1, y1, x2, y2 = hlines
+                        x3, y3, x4, y4 = vlines
+                        px, py = findIntersection(x1, y1, x2, y2, x3, y3, x4, y4)
+                        cv2.circle(frame, (int(px), int(py)), 5, (0, 0, 255), 5)
+                        points.append((int(px), int(py)))
                 frame = frame[30:930, 638:1567]
+                filtered = distance(points, 50)
                 cv2.imshow("frame", frame)
                 if cv2.waitKey(1) & 0xFF == ord('q'):
                     break
