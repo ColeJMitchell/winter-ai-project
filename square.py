d9357bdd80ffcbdf3feac38f1e4eb0ea803a4134
ColeJMitchell <colejmitchell.cs@gmail.com>
2025-01-07 21:07:44 -0500

    images of each square are now saved in dataset

--- square.py
+++ square.py
@@ -1,8 +1,18 @@
+import cv2
 class square:
     def __init__(self, x, y, num):
         self.x = x
         self.y = y
         self.num = num
     def diff(self):
-        print(self.num)
-        print(self.x[0]-self.y[0], self.x[1]-self.y[1])
+        xDiff = self.x[1] - self.x[0]
+        yDiff = self.y[1] - self.y[0]
+        print(f"Board num: {self.num}\n X dim: {xDiff}, Y dim: {yDiff}")
+    def findWrong(self):
+        xDiff = self.x[1] - self.x[0]
+        yDiff = self.y[1] - self.y[0]
+        if xDiff != 123 or yDiff != 114:
+            print(f"Board num: {self.num}\n X dim: {xDiff}, Y dim: {yDiff}")
+    def saveImage(self, image):
+        cv2.imwrite(f"dataset/square_{self.num}.jpg", image)
+            
