1f90f2e7e4b8576ba087b5e1a4c7d037b9f865dd
Cole <colejmitchell.cs@gmail.com>
2025-01-09 22:53:15 -0500

    more training data

--- brain.py
+++ brain.py
@@ -81,6 +81,82 @@ def initBoard():
         counter += 1
     return squares
 
+# Collect training data for the machine learning chess piece classification
+def collectData(board, squares):
+    board[0].saveImage(squares, "black", "rook", "1,1")
+    board[1].saveImage(squares, "black", "knight", "2,1")
+    board[2].saveImage(squares, "black", "bishop", "3,1")
+    board[3].saveImage(squares, "black", "king", "4,1")
+    board[4].saveImage(squares, "black", "queen", "5,1")
+    board[5].saveImage(squares, "black", "bishop", "6,1")
+    board[6].saveImage(squares, "black", "knight", "7,1")
+    board[7].saveImage(squares, "black", "rook", "8,1")
+    board[8].saveImage(squares, "black", "pawn", "1,2")
+    board[9].saveImage(squares, "black", "pawn", "2,2")
+    board[10].saveImage(squares, "black", "pawn", "3,2")
+    board[11].saveImage(squares, "black", "pawn", "4,2")
+    board[12].saveImage(squares, "black", "pawn", "5,2")
+    board[13].saveImage(squares, "black", "pawn", "6,2")
+    board[14].saveImage(squares, "black", "pawn", "7,2")
+    board[15].saveImage(squares, "black", "pawn", "8,2")
+    board[16].saveImage(squares, "empty", "square", "1,3")
+    board[17].saveImage(squares, "empty", "square", "2,3")
+    board[18].saveImage(squares, "empty", "square", "3,3")
+    board[19].saveImage(squares, "empty", "square", "4,3")
+    board[20].saveImage(squares, "empty", "square", "5,3")
+    board[21].saveImage(squares, "empty", "square", "6,3")
+    board[22].saveImage(squares, "empty", "square", "7,3")
+    board[23].saveImage(squares, "empty", "square", "8,3")
+    board[24].saveImage(squares, "empty", "square", "1,4")
+    board[25].saveImage(squares, "empty", "square", "2,4")
+    board[26].saveImage(squares, "empty", "square", "3,4")
+    board[27].saveImage(squares, "empty", "square", "4,4")
+    board[28].saveImage(squares, "empty", "square", "5,4")
+    board[29].saveImage(squares, "empty", "square", "6,4")
+    board[30].saveImage(squares, "empty", "square", "7,4")
+    board[31].saveImage(squares, "empty", "square", "8,4")
+    board[32].saveImage(squares, "empty", "square", "1,5")
+    board[33].saveImage(squares, "empty", "square", "2,5")
+    board[34].saveImage(squares, "empty", "square", "3,5")
+    board[35].saveImage(squares, "empty", "square", "4,5")
+    board[36].saveImage(squares, "empty", "square", "5,5")
+    board[37].saveImage(squares, "empty", "square", "6,5")
+    board[38].saveImage(squares, "empty", "square", "7,5")
+    board[39].saveImage(squares, "empty", "square", "8,5")
+    board[40].saveImage(squares, "empty", "square", "1,6")
+    board[41].saveImage(squares, "empty", "square", "2,6")
+    board[42].saveImage(squares, "empty", "square", "3,6")
+    board[43].saveImage(squares, "empty", "square", "4,6")
+    board[44].saveImage(squares, "empty", "square", "5,6")
+    board[45].saveImage(squares, "empty", "square", "6,6")
+    board[46].saveImage(squares, "empty", "square", "7,6")
+    board[47].saveImage(squares, "empty", "square", "8,6")
+    board[48].saveImage(squares, "white", "pawn", "1,7")
+    board[49].saveImage(squares, "white", "pawn", "2,7")
+    board[50].saveImage(squares, "white", "pawn", "3,7")
+    board[51].saveImage(squares, "white", "pawn", "4,7")
+    board[52].saveImage(squares, "white", "pawn", "5,7")
+    board[53].saveImage(squares, "white", "pawn", "6,7")
+    board[54].saveImage(squares, "white", "pawn", "7,7")
+    board[55].saveImage(squares, "white", "pawn", "8,7")
+    board[56].saveImage(squares, "white", "rook", "1,8")
+    board[57].saveImage(squares, "white", "knight", "2,8")
+    board[58].saveImage(squares, "white", "bishop", "3,8")
+    board[59].saveImage(squares, "white", "king", "4,8")
+    board[60].saveImage(squares, "white", "queen", "5,8")
+    board[61].saveImage(squares, "white", "bishop", "6,8")
+    board[62].saveImage(squares, "white", "knight", "7,8")
+    board[63].saveImage(squares, "white", "rook", "8,8")
+
+
+def collectRowData(board, squares, start, end, color, piece, col):
+    counter = 1
+    for i in range(start, end):
+        board[i].saveImage(squares, color, piece, f"{counter},{col}")
+        counter += 1
+    
+    
+
 # Connects to the server and processes the video feed
 def main():
     board = initBoard()
@@ -131,24 +207,9 @@ def main():
                     square.saveImage(squares)
                 return
                 '''
-                
-                board[0].saveImage(squares, "black", "rook", "1,1")
-                board[1].saveImage(squares, "black", "knight", "2,1")
-                board[2].saveImage(squares, "black", "bishop", "3,1")
-                board[3].saveImage(squares, "black", "king", "4,1")
-                board[4].saveImage(squares, "black", "queen", "5,1")
-                board[5].saveImage(squares, "black", "bishop", "6,1")
-                board[6].saveImage(squares, "black", "knight", "7,1")
-                board[7].saveImage(squares, "black", "rook", "8,1")
-                board[8].saveImage(squares, "black", "pawn", "1,2")
-                board[9].saveImage(squares, "black", "pawn", "2,2")
-                board[10].saveImage(squares, "black", "pawn", "3,2")
-                board[11].saveImage(squares, "black", "pawn", "4,2")
-                board[12].saveImage(squares, "black", "pawn", "5,2")
-                board[13].saveImage(squares, "black", "pawn", "6,2")
-                board[14].saveImage(squares, "black", "pawn", "7,2")
-                board[15].saveImage(squares, "black", "pawn", "8,2")
-              
+                collectRowData(board, squares, 45, 54, "white", "pawn", 7)
+                collectData(board, squares)
+                cv2.imshow("Frame", squares[22:136, 517:640])
                 if cv2.waitKey(1) & 0xFF == ord('q'):
                     break
                 
