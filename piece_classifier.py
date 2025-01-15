a40b56c9c724358266ae4aab01c1d2cf1b6ffb9e
Cole <colejmitchell.cs@gmail.com>
2025-01-15 01:13:14 -0500

    finished training the model on my collected dataset

--- piece_classifier.py
+++ piece_classifier.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 import torch.optim as optim
 import torchvision.transforms as transforms
 import torchvision.datasets as datasets
-from torch.utils.data import DataLoader, ConcatDataset
+from torch.utils.data import DataLoader
 import torch.nn.functional as F
 
 class chessAi(nn.Module):
@@ -13,7 +13,7 @@ class chessAi(nn.Module):
         self.conv2 = nn.Conv2d(16, 32, 3, padding=1)
         self.conv3 = nn.Conv2d(32, 64, 3, padding=1)
         self.maxpool = nn.MaxPool2d(2, 2)
-        self.linear2 = nn.Linear(64 * 8 * 8, 120)
+        self.linear2 = nn.Linear(64 * 16 * 16, 120)
         self.linear3 = nn.Linear(120, 84)
         self.linear4 = nn.Linear(84, 13)
     
@@ -21,11 +21,11 @@ class chessAi(nn.Module):
         x = self.maxpool(F.relu(self.conv1(x)))
         x = self.maxpool(F.relu(self.conv2(x)))
         x = self.maxpool(F.relu(self.conv3(x)))
-        x = self.maxpool(self.conv3(x))
         x = F.relu(self.linear2(torch.flatten(x, 1)))
         x = F.relu(self.linear3(x))
         return self.linear4(x)
         
+#Trains the convolutional neural network to classify chess pieces
 def main():
     
     transform = transforms.Compose([
@@ -33,38 +33,23 @@ def main():
     transforms.Resize((128, 128))
     ])
 
-    dataset = []
-    whitePawn = datasets.ImageFolder(root="training_data/white_pawn", transform=transform)
-    whiteRook = datasets.ImageFolder(root="training_data/white_rook", transform=transform)
-    whiteKnight = datasets.ImageFolder(root="training_data/white_knight", transform=transform)
-    whiteBishop = datasets.ImageFolder(root="training_data/white_bishop", transform=transform)
-    whiteQueen = datasets.ImageFolder(root="training_data/white_queen", transform=transform)
-    whiteKing = datasets.ImageFolder(root="training_data/white_king", transform=transform)
-    blackPawn = datasets.ImageFolder(root="training_data/black_pawn", transform=transform)
-    blackRook = datasets.ImageFolder(root="training_data/black_rook", transform=transform)
-    blackKnight = datasets.ImageFolder(root="training_data/black_knight", transform=transform)
-    blackBishop = datasets.ImageFolder(root="training_data/black_bishop", transform=transform)
-    blackQueen = datasets.ImageFolder(root="training_data/black_queen", transform=transform)
-    blackKing = datasets.ImageFolder(root="training_data/black_king", transform=transform)
-    emptySquare = datasets.ImageFolder(root="training_data/empty_square", transform=transform)
-    dataset.extend([whitePawn, whiteRook, whiteKnight, whiteBishop, whiteQueen, whiteKing, blackPawn, blackRook, blackKnight, blackBishop, blackQueen, blackKing, emptySquare])
-    dataset = ConcatDataset(dataset)
+    dataset = datasets.ImageFolder(root='training_data', transform=transform)
     dataloader = DataLoader(dataset, batch_size=32, shuffle=True)
-    
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model = chessAi().to(device)
     loss = nn.CrossEntropyLoss()
     optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
     
     for epoch in range(10):
+        running_loss = 0.0
         for i, (data, label) in enumerate(dataloader):
             data, label = data.to(device), label.to(device)
             optimizer.zero_grad()
             output = model(data)
-            lossVal = loss(output, label)
-            lossVal.backward()
+            loss_val = loss(output, label)
+            loss_val.backward()
             optimizer.step()
-            unning_loss += loss.item()
+            running_loss += loss_val.item()
             if i % 100 == 99:   
                 print(f'[Epoch {epoch + 1}, Batch {i + 1}] Loss: {running_loss / 100:.3f}')
                 running_loss = 0.0
